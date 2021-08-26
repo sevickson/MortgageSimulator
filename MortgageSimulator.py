e85bc8493c233c0217398bcb6f173ace562b822b
kwidamas <s.kwidama@dictu.nl>
2021-08-26 23:11:21 +0200

    rermove down payment

--- MortgageSimulator.py
+++ MortgageSimulator.py
@@ -26,21 +26,21 @@ with col1:
     interest_rate = st.number_input("Enter your home loan interest rate(%): ", min_value=0.0, format='%f')
 
 with col2:
-    st.subheader("Down Payment Percent")
-    down_payment_percent = st.number_input("Enter your down payment percent(%): ", min_value=0.0, format='%f')
+    #st.subheader("Down Payment Percent")
+    #down_payment_percent = st.number_input("Enter your down payment percent(%): ", min_value=0.0, format='%f')
     
     st.subheader("Target Payment Period (Years)")
     payment_years = st.number_input("Enter your target payment period (years): ", min_value=3, format='%d')
     
 
-down_payment = home_value* (down_payment_percent / 100)
-loan_amount = home_value - down_payment
+#down_payment = home_value* (down_payment_percent / 100)
+loan_amount = home_value #- down_payment
 payment_months = payment_years*12
 interest_rate = interest_rate / 100
 periodic_interest_rate = (1+interest_rate)**(1/12) - 1
 monthly_installment = -1*np.pmt(periodic_interest_rate , payment_months, loan_amount)
 
-st.subheader("**Down Payment:** $" + str(round(down_payment,2)))
+#st.subheader("**Down Payment:** $" + str(round(down_payment,2)))
 st.subheader("**Loan Amount:** $" + str(round(loan_amount, 2)))
 st.subheader("**Monthly Installment:** $" + str(round(monthly_installment, 2)))
 
